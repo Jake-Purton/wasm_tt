d7cef422193964144e1ddf14c774326022b790e5
h98177jp <jakecapurton@gmail.com>
2025-08-16 14:40:10 +0100

    random

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
 
 use core::f32;
-
 use bevy::{prelude::*, window::WindowResolution};
 use wasm_bindgen::prelude::*;
+use rand::{rngs::SmallRng, SeedableRng, Rng};
+
+const BOMB_COUNT: u32 = 40;
+const BOARD_W: usize = 16;
+const BOARD_H: usize = 16;
 
 #[wasm_bindgen(start)]
 pub fn start() {
@@ -15,12 +19,55 @@ pub fn start() {
             }),
             ..default()
         }))
+        .insert_resource(Board::new())
         .add_systems(Startup, setup)
         .add_systems(Update, (rotate_cube, move_cube))
         .run();
 }
 
+#[derive(Resource)]
+struct Board {
+    board: [[u8; BOARD_H]; BOARD_W],
+}
+
+impl Board {
+    pub fn new() -> Self {
+        
+        let mut board: [[u8; 16]; 16] = [[0; BOARD_H]; BOARD_W];
+        let mut vec: Vec<(usize, usize)> = Vec::new();
+
+        for x in 0..BOARD_W {
+            for y in 0..BOARD_H {
+                vec.push((x, y));
+            }
+        }
+
+        let mut rng = SmallRng::from_entropy();
+
+        for _ in 0..BOMB_COUNT {
+
+            let i = rng.gen_range(0..vec.len());
+            
+            let (x, y) = vec[i];
+
+            board[x][y] = 1;
+
+            vec.remove(i);
+        }
+
+        Self { board }
+
+    }
+}
+
 fn setup(mut commands: Commands) {
+
+    let mut rng = SmallRng::from_entropy();
+
+    // Example: randomize cube position
+    let x = rng.gen_range(-200.0..200.0);
+    let y = rng.gen_range(-200.0..200.0);
+
     commands.spawn(Camera2d::default());
     commands.spawn((Sprite {
             color: Color::srgb(0.3, 0.7, 0.9),
@@ -28,7 +75,7 @@ fn setup(mut commands: Commands) {
             ..default()
         },
         Transform {
-            translation: Vec3::new(0.0, 0.0, 1.0),
+            translation: Vec3::new(x, y, 1.0),
             ..Default::default()
         }
     ));
